2413ce76f8f952c0fca9ba63574667ef7d23757c
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-09-10 21:27:59 -0300

    Correção da formatação e indentação do código

--- src/trabalho1.cpp
+++ src/trabalho1.cpp
@@ -1,10 +1,10 @@
 /*
-  Name:        test01.2.Two.Windows.cpp
-  Author:      Rodrigo Luis de Souza da Silva
-  Author:      Bruno José Dembogurski
-  Date:        27/01/2011
-  Last Update: 27/01/2016
-  Description: Managing two windows using PixelLab.
+	Name: test01.2.Two.Windows.cpp
+	Author: Rodrigo Luis de Souza da Silva
+	Author: Bruno José Dembogurski
+	Date: 27/01/2011
+	Last Update: 27/01/2016
+	Description: Managing two windows using PixelLab.
 */
 
 #include "pixelLab.h"
@@ -15,248 +15,245 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CWIDTH 256
+#define CHEIGHT 256
+
 // Image Objects
 PixelLab *img = NULL, *imgOriginal = NULL;
 int window1 = 0;
 int window2 = 0;
 int brightnessSlider = 128;
 
-void idle()
-{
-   glutPostRedisplay();
+void idle() {
+	glutPostRedisplay();
 }
 
-static void display1(void)
-{
-   glClearColor (1.0, 1.0, 1.0, 0.0);
-   glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+static void display1(void) {
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   // reverse 'Y' to allow correct mouse click (0 value is in the upper left corner for mouse functions)
-   gluOrtho2D(0, 256, 256, 0); // You could use "glOrtho(0, 64, 256, 0, -5, 5);" instead if you want 3D features
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// reverse 'Y' to allow correct mouse click (0 value is in the upper left corner for mouse functions)
+	gluOrtho2D(0, CWIDTH, CHEIGHT, 0); // You could use "glOrtho(0, 64, 256, 0, -5, 5);" instead if you want 3D features
 
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-   // left vertical
-   glBegin(GL_QUADS);
-      glColor3f(0.0f, 0.0f, 0.0f);
-      glVertex2d(0, 0);
-      glVertex2d(16, 0);
-      glVertex2d(16, 232);
-      glVertex2d(0, 232);
-   glEnd();
+	// left vertical
+	glBegin(GL_QUADS);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex2d(0, 0);
+		glVertex2d(16, 0);
+		glVertex2d(16, 232);
+		glVertex2d(0, 232);
+	glEnd();
 
-   glBegin(GL_QUADS);
-      glColor3f(0.0f, 0.0f, 0.0f);
-      glVertex2d( 1, 1);
-      glVertex2d(15, 1);
-      glColor3f(1.0f, 1.0f, 1.0f);
-      glVertex2d(15, 231);
-      glVertex2d( 1, 231);
-   glEnd();
+	glBegin(GL_QUADS);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex2d( 1, 1);
+		glVertex2d(15, 1);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glVertex2d(15, 231);
+		glVertex2d( 1, 231);
+	glEnd();
 
-   glBegin(GL_LINES);
-      glColor3f(1.0f, 0.0f, 0.0f);
-      glVertex2d( 1, brightnessSlider);
-      glVertex2d(15, brightnessSlider);
-   glEnd();
+	glBegin(GL_LINES);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2d( 1, brightnessSlider);
+		glVertex2d(15, brightnessSlider);
+	glEnd();
 
-   // bottom horizontal
-      glBegin(GL_QUADS);
-      glColor3f(0.0f, 0.0f, 0.0f);
-      glVertex2d(24, 240);
-      glVertex2d(24, 256);
-      glVertex2d(256, 256);
-      glVertex2d(256, 240);
-   glEnd();
+	// bottom horizontal
+		glBegin(GL_QUADS);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex2d(24, 240);
+		glVertex2d(24, 256);
+		glVertex2d(256, 256);
+		glVertex2d(256, 240);
+	glEnd();
 
-   glBegin(GL_QUADS);
-      glColor3f(1.0f, 1.0f, 1.0f);
-      glVertex2d(25, 241);
-      glVertex2d(25, 255);
-      glColor3f(0.0f, 0.0f, 0.0f);
-      glVertex2d(255, 255);
-      glVertex2d(255, 241);
-   glEnd();
+	glBegin(GL_QUADS);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glVertex2d(25, 241);
+		glVertex2d(25, 255);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex2d(255, 255);
+		glVertex2d(255, 241);
+	glEnd();
 
-   // center square
-   glBegin(GL_QUADS);
-   	  glColor3f(0.0f, 0.0f, 0.0f);
-   	  glVertex2d(24, 0);
-	  glVertex2d(256, 0);
-	  glVertex2d(256, 232);
-	  glVertex2d(24, 232);
-   glEnd();
+	// center square
+	glBegin(GL_QUADS);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex2d(24, 0);
+		glVertex2d(256, 0);
+		glVertex2d(256, 232);
+		glVertex2d(24, 232);
+	glEnd();
 
-   glBegin(GL_QUADS);
-	  glColor3f(0.94f, 0.94f, 0.94f);
-	  glVertex2d(25, 1);
-	  glVertex2d(255, 1);
-	  glVertex2d(255, 231);
-	  glVertex2d(25, 231);
-   glEnd();
+	glBegin(GL_QUADS);
+		glColor3f(0.94f, 0.94f, 0.94f);
+		glVertex2d(25, 1);
+		glVertex2d(255, 1);
+		glVertex2d(255, 231);
+		glVertex2d(25, 231);
+	glEnd();
 
-   // dashed lines
-   glEnable(GL_LINE_STIPPLE);
-   glColor3f(0, 0, 0);
-   glLineStipple(2, 0xAAAA);
-   glLineWidth(1);
+	// dashed lines
+	glEnable(GL_LINE_STIPPLE);
+	glColor3f(0, 0, 0);
+	glLineStipple(2, 0xAAAA);
+	glLineWidth(1);
 
-   // vertical
-   glBegin(GL_LINES);
-	  glVertex2f(82, 1);
-	  glVertex2f(82, 231);
-   glEnd();
+	// vertical
+	glBegin(GL_LINES);
+		glVertex2f(82, 1);
+		glVertex2f(82, 231);
+	glEnd();
 
-   glBegin(GL_LINES);
-   	  glVertex2f(140, 1);
-   	  glVertex2f(140, 231);
-   glEnd();
+	glBegin(GL_LINES);
+		glVertex2f(140, 1);
+		glVertex2f(140, 231);
+	glEnd();
 
-   glBegin(GL_LINES);
-      glVertex2f(198, 1);
-      glVertex2f(198, 231);
-   glEnd();
+	glBegin(GL_LINES);
+		glVertex2f(198, 1);
+		glVertex2f(198, 231);
+	glEnd();
 
-   // horizontal
-   glBegin(GL_LINES);
-	  glVertex2f(25, 58);
-	  glVertex2f(255, 58);
-   glEnd();
+	// horizontal
+	glBegin(GL_LINES);
+		glVertex2f(25, 58);
+		glVertex2f(255, 58);
+	glEnd();
 
-   glBegin(GL_LINES);
-   	  glVertex2f(25, 116);
-   	  glVertex2f(255, 116);
-   glEnd();
+	glBegin(GL_LINES);
+		glVertex2f(25, 116);
+		glVertex2f(255, 116);
+	glEnd();
 
-   glBegin(GL_LINES);
-      glVertex2f(25, 174);
-      glVertex2f(255, 174);
-   glEnd();
+	glBegin(GL_LINES);
+		glVertex2f(25, 174);
+		glVertex2f(255, 174);
+	glEnd();
 
-   glDisable(GL_LINE_STIPPLE);
+	glDisable(GL_LINE_STIPPLE);
 
-   glutSwapBuffers ();
+	glutSwapBuffers();
 }
 
-static void display2(void)
-{
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   img->ViewImage();
-   glutSwapBuffers();
+static void display2(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	img->ViewImage();
+	glutSwapBuffers();
 }
 
-void modifyImage()
-{
-   int value = brightnessSlider-128; // To make the middle of the slider, zero
-   int pixelVal;
+void modifyImage() {
+	int value = brightnessSlider - 128; // To make the middle of the slider, zero
+	int pixelVal;
 
-   for(int y = 0; y < imgOriginal->GetHeight(); y++)
-      for(int x = 0; x < imgOriginal->GetWidth(); x++)
-      {
-         pixelVal = imgOriginal->GetGrayValue(x, y);
-         pixelVal += value;
-         if(pixelVal >= 255) pixelVal = 255;
-         if(pixelVal < 0)    pixelVal = 0;
-         img->SetGrayValue(x, y, (uByte) pixelVal);
-      }
-   // Update both windows
-   glutPostWindowRedisplay	(window1);
-   glutPostWindowRedisplay	(window2);
+	for (int y = 0; y < imgOriginal->GetHeight(); y++) {
+		for (int x = 0; x < imgOriginal->GetWidth(); x++) {
+			pixelVal = imgOriginal->GetGrayValue(x, y);
+			pixelVal += value;
+			if (pixelVal >= 255) pixelVal = 255;
+			if (pixelVal < 0) pixelVal = 0;
+			img->SetGrayValue(x, y, (uByte) pixelVal);
+		}
+	}
+
+	// Update both windows
+	glutPostWindowRedisplay	(window1);
+	glutPostWindowRedisplay	(window2);
 }
 
 // Keyboard
-static void key(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-      case 27 :
-      case 'q':
-         exit(0);
-      break;
-    }
-    glutPostRedisplay();
+static void key(unsigned char key, int x, int y) {
+	 switch (key) {
+		case 27 :
+
+		case 'q':
+			exit(0);
+		break;
+	 }
+
+	 glutPostRedisplay();
 }
 
 // Special Keys callback
-void specialKeys(int key, int x, int y)
-{
-   switch(key)
-   {
-      case GLUT_KEY_UP:
-         brightnessSlider = (brightnessSlider<0) ? 0 : brightnessSlider-1;
-         modifyImage();
-      break;
-      case GLUT_KEY_DOWN:
-         brightnessSlider = (brightnessSlider>255) ? 255 : brightnessSlider+1;
-         modifyImage();
-      break;
-      case 'q':
-    	  exit(0);
-   }
-   glutPostRedisplay();
+void specialKeys(int key, int x, int y) {
+	switch(key) {
+		case GLUT_KEY_UP:
+			brightnessSlider = (brightnessSlider<0) ? 0 : brightnessSlider-1;
+			modifyImage();
+		break;
+
+		case GLUT_KEY_DOWN:
+			brightnessSlider = (brightnessSlider>255) ? 255 : brightnessSlider+1;
+			modifyImage();
+		break;
+
+		case 'q':
+			exit(0);
+	}
+
+	glutPostRedisplay();
 }
 
 // Mouse callback - Capture mouse click in the brightness window
-void mouse(int button, int state, int x, int y)
-{
-   if ( button == GLUT_LEFT_BUTTON)
-      if(state == GLUT_DOWN)
-         brightnessSlider = y;
-   modifyImage();
+void mouse(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON)
+		if (state == GLUT_DOWN)
+			brightnessSlider = y;
+	modifyImage();
 }
 
 // Motion callback - Capture mouse motion when left button is clicked
-void motion(int x, int y )
-{
-   brightnessSlider = y;
-   modifyImage();
+void motion(int x, int y ) {
+	brightnessSlider = y;
+	modifyImage();
 }
 
-void init()
-{
-   imgOriginal = new PixelLab();
-   imgOriginal->Read("figs/lenaGray.png");
+void init() {
+	imgOriginal = new PixelLab();
+	imgOriginal->Read("figs/lenaGray.png");
 
-   img = new PixelLab();
-   img->Copy(imgOriginal);
+	img = new PixelLab();
+	img->Copy(imgOriginal);
 
-   printf("Change brightness clicking on the left window\n or using the 'up' and 'down' keyboard keys.\n");
+	printf("Change brightness clicking on the left window\n or using the 'up' and 'down' keyboard keys.\n");
 }
 
-int main(int argc, char *argv[])
-{
-   glutInit(&argc, argv);
-   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+int main(int argc, char *argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-   // Init image objects
-   init();
+	// Init image objects
+	init();
 
-   // Create two windows
-   window2 = glutCreateWindow("Display Window");
-   window1 = glutCreateWindow("Control Window");
+	// Create two windows
+	window2 = glutCreateWindow("Display Window");
+	window1 = glutCreateWindow("Control Window");
 
-   ///////////////////////////////////////////////////////////
-   // Control Window
-   glutSetWindow (window1);   // Change current window to 1 (all callbacks will be set to this window)
-   glutDisplayFunc(display1);
-   glutPositionWindow(20, 30);
-   glutReshapeWindow(256, 256);
-   glutMouseFunc( mouse );
-   glutMotionFunc( motion );
-   glutSpecialFunc(specialKeys);
-   glutKeyboardFunc(key);
+	///////////////////////////////////////////////////////////
+	// Control Window
+	glutSetWindow(window1);	// Change current window to 1 (all callbacks will be set to this window)
+	glutDisplayFunc(display1);
+	glutPositionWindow(20, 30);
+	glutReshapeWindow(CWIDTH, CHEIGHT);
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
+	glutSpecialFunc(specialKeys);
+	glutKeyboardFunc(key);
 
-   ///////////////////////////////////////////////////////////
-   // Display Window
-   glutSetWindow (window2); // Change current window to 2
-   glutDisplayFunc(display2);
-   glutReshapeWindow(imgOriginal->GetWidth(),imgOriginal->GetHeight());
-   glutPositionWindow(286, 30);
+	///////////////////////////////////////////////////////////
+	// Display Window
+	glutSetWindow(window2); // Change current window to 2
+	glutDisplayFunc(display2);
+	glutReshapeWindow(imgOriginal->GetWidth(),imgOriginal->GetHeight());
+	glutPositionWindow(286, 30);
 
-   glutMainLoop();
+	glutMainLoop();
 
-   return 0;
+	return 0;
 }
