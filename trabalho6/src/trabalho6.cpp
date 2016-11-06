9ec8e11efbbd3c6ad786e4b2c18c270c3f908ddb
Ronaldo Vieira <ronaldo.vieira@ufrrj.br>
2016-11-06 19:48:05 -0200

    Adição de suporte para imagens coloridas

--- trabalho6/src/trabalho6.cpp
+++ trabalho6/src/trabalho6.cpp
@@ -32,7 +32,7 @@ int prevBrightness = 0;
 char input[512];
 char output[512];
 
-COMPLEX **m;
+COMPLEX **m, **mr, **mg, **mb;
 
 bool isTransformed = false;
 
@@ -77,17 +77,36 @@ bool isWithinBounds(int x, int y) {
 
 void initMatrix() {
 	m = (COMPLEX**) malloc(sizeof(COMPLEX*) * imgMod->GetWidth());
+	mr = (COMPLEX**) malloc(sizeof(COMPLEX*) * imgMod->GetWidth());
+	mg = (COMPLEX**) malloc(sizeof(COMPLEX*) * imgMod->GetWidth());
+	mb = (COMPLEX**) malloc(sizeof(COMPLEX*) * imgMod->GetWidth());
 
 	for (int i = 0; i < imgMod->GetWidth(); i++) {
 		m[i] = (COMPLEX*) malloc(sizeof(COMPLEX) * imgMod->GetHeight());
+		mr[i] = (COMPLEX*) malloc(sizeof(COMPLEX) * imgMod->GetHeight());
+		mg[i] = (COMPLEX*) malloc(sizeof(COMPLEX) * imgMod->GetHeight());
+		mb[i] = (COMPLEX*) malloc(sizeof(COMPLEX) * imgMod->GetHeight());
 	}
 }
 
 void updateMatrix() {
-	for (int y = 0; y < imgMod->GetHeight(); y++) {
-		for (int x = 0; x < imgMod->GetWidth(); x++) {
-			m[x][y].real = imgMod->GetGrayValue(x, y);
-			m[x][y].imag = 0;
+	if (imgMod->GetNumberOfChannels() == 1) {
+		for (int y = 0; y < imgMod->GetHeight(); y++) {
+			for (int x = 0; x < imgMod->GetWidth(); x++) {
+				m[x][y].real = imgMod->GetGrayValue(x, y);
+				m[x][y].imag = 0;
+			}
+		}
+	} else {
+		for (int y = 0; y < imgMod->GetHeight(); y++) {
+			for (int x = 0; x < imgMod->GetWidth(); x++) {
+				mr[x][y].real = imgMod->GetR(x, y);
+				mr[x][y].imag = 0;
+				mg[x][y].real = imgMod->GetG(x, y);
+				mg[x][y].imag = 0;
+				mb[x][y].real = imgMod->GetB(x, y);
+				mb[x][y].imag = 0;
+			}
 		}
 	}
 }
@@ -131,21 +150,48 @@ static void display(void) {
 
 void applyFFT() {
 	if (img) {
-		if (imgMod->GetNumberOfChannels() == 3) imgMod->ConvertToGrayScale();
+		if (imgMod->GetNumberOfChannels() == 1) {
+			FFT2D(m, imgMod->GetWidth(), imgMod->GetHeight(), 1);
 
-		FFT2D(m, imgMod->GetWidth(), imgMod->GetHeight(), 1);
+			for (int y = 0; y < imgMod->GetHeight(); y++) {
+				for (int x = 0; x < imgMod->GetWidth(); x++) {
+					double value = sqrt(pow(m[x][y].real, 2) + pow(m[x][y].imag, 2)) * 200;
 
-		for (int y = 0; y < imgMod->GetHeight(); y++) {
-			for (int x = 0; x < imgMod->GetWidth(); x++) {
-				double value = sqrt(pow(m[x][y].real, 2) + pow(m[x][y].imag, 2)) * 200;
+					// adiciona um valor arbitrário
+					if (value > 0) value += FOURIER_OFFSET;
 
-				// adiciona um valor arbitrário
-				if (value > 0) value += FOURIER_OFFSET;
+					if (value > 255) value = 255;
+					if (value < 0) value = 0;
 
-				if (value > 255) value = 255;
-				if (value < 0) value = 0;
+					imgMod->SetGrayValue(x, y, value);
+				}
+			}
+		} else {
+			FFT2D(mr, imgMod->GetWidth(), imgMod->GetHeight(), 1);
+			FFT2D(mg, imgMod->GetWidth(), imgMod->GetHeight(), 1);
+			FFT2D(mb, imgMod->GetWidth(), imgMod->GetHeight(), 1);
 
-				imgMod->SetGrayValue(x, y, value);
+			for (int y = 0; y < imgMod->GetHeight(); y++) {
+				for (int x = 0; x < imgMod->GetWidth(); x++) {
+					double valueR = sqrt(pow(mr[x][y].real, 2) + pow(mr[x][y].imag, 2)) * 200;
+					double valueG = sqrt(pow(mg[x][y].real, 2) + pow(mg[x][y].imag, 2)) * 200;
+					double valueB = sqrt(pow(mb[x][y].real, 2) + pow(mb[x][y].imag, 2)) * 200;
+
+					// adiciona um valor arbitrário
+					if (valueR > 0) valueR += FOURIER_OFFSET;
+					if (valueG > 0) valueG += FOURIER_OFFSET;
+					if (valueB > 0) valueB += FOURIER_OFFSET;
+
+					if (valueR > 255) valueR = 255;
+					if (valueG > 255) valueG = 255;
+					if (valueB > 255) valueB = 255;
+
+					if (valueR < 0) valueR = 0;
+					if (valueG < 0) valueG = 0;
+					if (valueB < 0) valueB = 0;
+
+					imgMod->SetRGB(x, y, valueR, valueG, valueB);
+				}
 			}
 		}
 	}
@@ -153,18 +199,40 @@ void applyFFT() {
 
 void applyIFFT() {
 	if (img) {
-		if (imgMod->GetNumberOfChannels() == 3) imgMod->ConvertToGrayScale();
+		if (imgMod->GetNumberOfChannels() == 1) {
+			FFT2D(m, imgMod->GetWidth(), imgMod->GetHeight(), -1);
 
-		FFT2D(m, imgMod->GetWidth(), imgMod->GetHeight(), -1);
+			for (int y = 0; y < imgMod->GetHeight(); y++) {
+				for (int x = 0; x < imgMod->GetWidth(); x++) {
+					double value = sqrt(pow(m[x][y].real, 2) + pow(m[x][y].imag, 2));
 
-		for (int y = 0; y < imgMod->GetHeight(); y++) {
-			for (int x = 0; x < imgMod->GetWidth(); x++) {
-				double value = sqrt(pow(m[x][y].real, 2) + pow(m[x][y].imag, 2));
+					if (value > 255) value = 255;
+					if (value < 0) value = 0;
 
-				if (value > 255) value = 255;
-				if (value < 0) value = 0;
+					imgMod->SetGrayValue(x, y, value);
+				}
+			}
+		} else {
+			FFT2D(mr, imgMod->GetWidth(), imgMod->GetHeight(), -1);
+			FFT2D(mg, imgMod->GetWidth(), imgMod->GetHeight(), -1);
+			FFT2D(mb, imgMod->GetWidth(), imgMod->GetHeight(), -1);
 
-				imgMod->SetGrayValue(x, y, value);
+			for (int y = 0; y < imgMod->GetHeight(); y++) {
+				for (int x = 0; x < imgMod->GetWidth(); x++) {
+					double valueR = sqrt(pow(mr[x][y].real, 2) + pow(mr[x][y].imag, 2));
+					double valueG = sqrt(pow(mg[x][y].real, 2) + pow(mg[x][y].imag, 2));
+					double valueB = sqrt(pow(mb[x][y].real, 2) + pow(mb[x][y].imag, 2));
+
+					if (valueR > 255) valueR = 255;
+					if (valueG > 255) valueG = 255;
+					if (valueB > 255) valueB = 255;
+
+					if (valueR < 0) valueR = 0;
+					if (valueG < 0) valueG = 0;
+					if (valueB < 0) valueB = 0;
+
+					imgMod->SetRGB(x, y, valueR, valueG, valueB);
+				}
 			}
 		}
 	}
@@ -202,6 +270,7 @@ void applySaltAndPepper() {
 	if (img) {
 		pixel **mat;
 		double num;
+		int channels = imgMod->GetNumberOfChannels();
 
 		imgMod->AllocatePixelMatrix(&mat, imgMod->GetHeight(), imgMod->GetWidth());
 		imgMod->GetDataAsMatrix(mat);
@@ -213,7 +282,13 @@ void applySaltAndPepper() {
 				if (num < noiseProb) {
 					num = (double) std::rand() / RAND_MAX;
 
-					mat[y][x].value = num < 0.5? 0 : 255;
+					if (channels == 1) {
+						mat[y][x].value = num < 0.5? 0 : 255;
+					} else {
+						mat[y][x].R = num < 0.5? 0 : 255;
+						mat[y][x].G = num < 0.5? 0 : 255;
+						mat[y][x].B = num < 0.5? 0 : 255;
+					}
 				}
 			}
 		}
@@ -226,29 +301,63 @@ void applySaltAndPepper() {
 void applyMedianFilter() {
 	if (img) {
 		pixel **mat;
-		int nbh[9] = {0};
 		int i, j;
 
 		imgMod->AllocatePixelMatrix(&mat, imgMod->GetHeight(), imgMod->GetWidth());
 		imgMod->GetDataAsMatrix(mat);
 
-		for (int y = 0; y < imgMod->GetHeight(); y++) {
-			for (int x = 0; x < imgMod->GetWidth(); x++) {
+		if (imgMod->GetNumberOfChannels() == 1) {
+			int nbh[9] = {0};
 
-				for (int dy = 0; dy < 3; dy++) {
-					for (int dx = 0; dx < 3; dx++) {
-						i = x + dx - 1;
-						j = y + dy - 1;
+			for (int y = 0; y < imgMod->GetHeight(); y++) {
+				for (int x = 0; x < imgMod->GetWidth(); x++) {
 
-						if (i >= 0 && i < imgMod->GetWidth() &&
-							j >= 0 && j < imgMod->GetHeight()) {
-							nbh[dx + dy * 3] = mat[y + dy - 1][x + dx - 1].value;
+					for (int dy = 0; dy < 3; dy++) {
+						for (int dx = 0; dx < 3; dx++) {
+							i = x + dx - 1;
+							j = y + dy - 1;
+
+							if (i >= 0 && i < imgMod->GetWidth() &&
+								j >= 0 && j < imgMod->GetHeight()) {
+								nbh[dx + dy * 3] = mat[y + dy - 1][x + dx - 1].value;
+							}
 						}
 					}
-				}
 
-				std::sort(nbh, nbh + 9);
-				mat[y][x].value = nbh[4];
+					std::sort(nbh, nbh + 9);
+					mat[y][x].value = nbh[4];
+				}
+			}
+		} else {
+			int nbhr[9] = {0};
+			int nbhg[9] = {0};
+			int nbhb[9] = {0};
+
+			for (int y = 0; y < imgMod->GetHeight(); y++) {
+				for (int x = 0; x < imgMod->GetWidth(); x++) {
+
+					for (int dy = 0; dy < 3; dy++) {
+						for (int dx = 0; dx < 3; dx++) {
+							i = x + dx - 1;
+							j = y + dy - 1;
+
+							if (i >= 0 && i < imgMod->GetWidth() &&
+								j >= 0 && j < imgMod->GetHeight()) {
+								nbhr[dx + dy * 3] = mat[y + dy - 1][x + dx - 1].R;
+								nbhg[dx + dy * 3] = mat[y + dy - 1][x + dx - 1].G;
+								nbhb[dx + dy * 3] = mat[y + dy - 1][x + dx - 1].G;
+							}
+						}
+					}
+
+					std::sort(nbhr, nbhr + 9);
+					std::sort(nbhg, nbhg + 9);
+					std::sort(nbhb, nbhb + 9);
+
+					mat[y][x].R = nbhr[4];
+					mat[y][x].G = nbhg[4];
+					mat[y][x].B = nbhb[4];
+				}
 			}
 		}
 
@@ -263,13 +372,30 @@ void applyNotchFilter(int radio, int centerx, int centery) {
 	imgMod->AllocatePixelMatrix(&mat, imgMod->GetWidth(), imgMod->GetHeight());
 	imgMod->GetDataAsMatrix(mat);
 
-	for (int j = centery - radio; j < centery + radio; j++) {
-		for (int i = centerx - radio; i < centerx + radio; i++) {
-			if (isWithin(radio, i, j, centerx, centery)
-					&& isWithinBounds(i, j)) {
-				mat[j][i].value = FOURIER_OFFSET;
-				m[i][j].real = 0;
-				m[i][j].imag = 0;
+	if (imgMod->GetNumberOfChannels() == 1) {
+		for (int j = centery - radio; j < centery + radio; j++) {
+			for (int i = centerx - radio; i < centerx + radio; i++) {
+				if (isWithin(radio, i, j, centerx, centery)
+						&& isWithinBounds(i, j)) {
+					mat[j][i].value = FOURIER_OFFSET;
+					m[i][j].real = 0;
+					m[i][j].imag = 0;
+				}
+			}
+		}
+	} else {
+		for (int j = centery - radio; j < centery + radio; j++) {
+			for (int i = centerx - radio; i < centerx + radio; i++) {
+				if (isWithin(radio, i, j, centerx, centery)
+						&& isWithinBounds(i, j)) {
+					mat[j][i].R = FOURIER_OFFSET;
+					mat[j][i].G = FOURIER_OFFSET;
+					mat[j][i].B = FOURIER_OFFSET;
+
+					mr[i][j].real = 0; mr[i][j].imag = 0;
+					mg[i][j].real = 0; mg[i][j].imag = 0;
+					mb[i][j].real = 0; mb[i][j].imag = 0;
+				}
 			}
 		}
 	}
@@ -359,7 +485,7 @@ static void mouse(int button, int state, int x, int y) {
 int main(int argc, char *argv[]) {
 	std::srand(std::time(NULL));
 	glutInit(&argc, argv);
-	strcpy(input, "figs/lenaGray.png");
+	strcpy(input, "figs/lena.png");
 	strcpy(output, "figs/output.png");
 
 	img = new PixelLab(input);
